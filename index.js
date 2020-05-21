042b109ccd2fe7d2979c6aeb22409f210f8ece79
Chris Sanchez <chrisvsanchez@gmail.com>
2020-05-21 13:42:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,7 +62,6 @@ function placeOrder(cardNumber) {
   if(typeof cardNumber  !== "number"){
     return "Sorry, we don't have a credit card on file for you."
   }else{
-    let cart = [];
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     let cart = [];
     return cart;
