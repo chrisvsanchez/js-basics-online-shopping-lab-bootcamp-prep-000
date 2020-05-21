fb6e55e272c88ddd6fe0056175df0f7d44983015
Chris Sanchez <chrisvsanchez@gmail.com>
2020-05-21 13:37:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function placeOrder(cardNumber) {
   }else{
     // return("Your total cost is $" + total() + ", which will be charged to the card " + `${cardNumber}` + ".");
     return`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-    let cart == [];
+    let cart = [];
   }
 
 }
