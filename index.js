0dbe34b8b7320466d6161b2110b5f909c81ed73e
Chris Sanchez <chrisvsanchez@gmail.com>
2020-05-21 13:50:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,6 +66,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }else{
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    let cart = [];
+    return cart;
   }
   
 }
