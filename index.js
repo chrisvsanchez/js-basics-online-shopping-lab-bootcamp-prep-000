51a9f92e24c3e3f42284cc90fe053785f001a762
Chris Sanchez <chrisvsanchez@gmail.com>
2020-05-21 13:49:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if(typeof cardNumber  !== "number"){
     return "Sorry, we don't have a credit card on file for you."
-  }else if (typeof cardNumber === "number"){
+  }else{
     return`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
-  return emptyCart();
+  
 }
